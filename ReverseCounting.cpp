b37f3ef9386ca0611fa0110950c110e83819b2b0
Rahimullah Khan <rahimullahmehsud148@gmail.com>
2026-06-21 22:28:40 +0500

    Day 6 Task: Reverse Counting num to 1

---
+++ ReverseCounting.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+    cout<<"Reverse counting from "<<num<<" is: ";
+    for(int i=num;i>=1;i--)
+{cout<<i<< " ";}
+return 0;
+
+}
