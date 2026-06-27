e4aa22c07b583249e0d6fb31c94d83daf44e5ccc
Rahimullah Khan <rahimullahmehsud148@gmail.com>
2026-06-27 20:49:56 +0500

    Day 10 Task: Number_Square.cpp

---
+++ Number_Square.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num;
+cout<<"Enter number to print :";
+cin>>num;
+for(int i=1;i<=num;i++)
+{
+    for(int j=1;j<=num;j++)
+    {
+        cout<<i;    
+ } cout<<endl; 
+}
+return 0;
+}
